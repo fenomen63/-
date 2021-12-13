8df948578cb099dd544cec72f1d68bd0cfc1636d
student <student@05-15>
2021-12-13 20:50:55 +0200

    еще добавил

--- ConsoleApplication1/ConsoleApplication1/Rectangle.h
+++ ConsoleApplication1/ConsoleApplication1/Rectangle.h
@@ -1,5 +1,25 @@
 #pragma once
-class Rectangle
+#include "Square.h"
+#include "Line.h"
+class c_Rectangle
 {
+private:
+	c_Square a;
+	c_Line b;
+    int size;
+public:
+    c_Rectangle();
+    c_Rectangle(int size);
+    c_Rectangle(c_Rectangle& rectangle)
+    {
+        
+    }
+    void info() const {
+        std::cout << "size: " << size;
+    }
+    int GetSize()
+    {
+        return size;
+    }
 };
 
--- ConsoleApplication1/ConsoleApplication1/Stiralka.h
+++ ConsoleApplication1/ConsoleApplication1/Stiralka.h
@@ -1,5 +1,24 @@
 #pragma once
-class Stiralka
+#include "Square.h"
+#include "Circle.h"
+class c_Stiralka
 {
-};
+private:
+    c_Square a;
+    c_Circle b;
+    int size;
+public:
+    c_Stiralka();
+    c_Stiralka(int size);
+    c_Stiralka(c_Stiralka& stiralka)
+    {
 
+    }
+    void info() const {
+        std::cout << "size: " << size;
+    }
+    int GetSize()
+    {
+        return size;
+    }
+};
